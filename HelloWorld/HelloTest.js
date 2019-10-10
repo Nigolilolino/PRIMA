09114af4cb8659d3a7ed2988d1943fe4578d1dca
34275116 <34275116+NicoKlein97@users.noreply.github.com>
2019-10-10 12:27:01 +0200

    Hello World erstellt

---
+++ HelloWorld/HelloTest.js
@@ -0,0 +1,12 @@
+"use strict";
+console.log("Hello World");
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("test").innerHTML = "Hello World";
+    document.body.innerHTML = "Eyoooooo";
+});
+//window.addEventListener("load", handleLoad);
+//function handleLoad(_event: Event){
+//  document.body.innerHTML = "Eyoooooo";
+//}
+//Coding Style ansehen
+//# sourceMappingURL=HelloTest.js.map
---
+++ HelloWorld/HelloTest.ts
@@ -0,0 +1,16 @@
+console.log("Hello World");
+//document.addEventListener("DOMContentLoaded", function () {
+    //document.getElementById("test").innerHTML = "Hello World";
+
+  //  document.body.innerHTML = "Eyoooooo";
+//});
+
+window.addEventListener("load", handleLoad);
+
+function handleLoad(_event: Event){
+    document.body.innerHTML = "Eyoooooo";
+}
+
+
+
+//Coding Style ansehen
