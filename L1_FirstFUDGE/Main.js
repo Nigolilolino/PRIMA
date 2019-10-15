888792b430720596660e61b5730ac9c39db7e167
34275116 <34275116+NicoKlein97@users.noreply.github.com>
2019-10-15 18:24:21 +0200

    L1 Funktionsfähig

--- L1_FirstFUDGE/Main.js
+++ L1_FirstFUDGE/Main.js
@@ -1,13 +1,33 @@
 "use strict";
+///<reference types="../FUDGE/Build/FudgeCore.js"/>
 var L1_FirstFUDGE;
+///<reference types="../FUDGE/Build/FudgeCore.js"/>
 (function (L1_FirstFUDGE) {
     var fudge = FudgeCore;
     window.addEventListener("load", handleLoad);
     function handleLoad(_event) {
         let canvas = document.querySelector("canvas");
+        fudge.RenderManager.initialize();
+        fudge.Debug.log(canvas);
         console.log(canvas);
+        let node = new fudge.Node("Quad");
+        //Erstellung eines Meshs und dessen Komponente
+        let mesh = new fudge.MeshQuad();
+        let cmpMesh = new fudge.ComponentMesh(mesh);
+        //Erstellung eines Materials und dessen Komponente
+        let material = new fudge.Material("Solidwhite", fudge.ShaderUniColor, new fudge.CoatColored(new fudge.Color(1, 0, 0, 1)));
+        let cmpMaterial = new fudge.ComponentMaterial(material);
+        //sVerknüpfung der Komonenten mit dem Knoten
+        node.addComponent(cmpMesh);
+        node.addComponent(cmpMaterial);
+        //Kamera Komponente
+        let cmpCamera = new fudge.ComponentCamera();
         let viewport = new fudge.Viewport;
-        viewport.initialize("Viewport", null, null, canvas);
+        viewport.initialize("camera", node, cmpCamera, canvas);
+        //Verschiebung der Kamera
+        viewport.camera.pivot.translateZ(2);
+        fudge.Debug.log(viewport);
+        viewport.draw();
     }
 })(L1_FirstFUDGE || (L1_FirstFUDGE = {}));
 //# sourceMappingURL=Main.js.map
--- L1_FirstFUDGE/Main.ts
+++ L1_FirstFUDGE/Main.ts
@@ -1,17 +1,39 @@
+///<reference types="../FUDGE/Build/FudgeCore.js"/>
 namespace L1_FirstFUDGE {
+
     import fudge = FudgeCore;
 
     window.addEventListener("load", handleLoad);
 
     function handleLoad(_event: Event): void {
         let canvas: HTMLCanvasElement = document.querySelector("canvas");
-
+        fudge.RenderManager.initialize();
+        fudge.Debug.log(canvas);
         console.log(canvas);   
 
+        let node: fudge.Node = new fudge.Node("Quad");
+
+        //Erstellung eines Meshs und dessen Komponente
+        let mesh: fudge.MeshQuad = new fudge.MeshQuad();
+        let cmpMesh: fudge.ComponentMesh = new fudge.ComponentMesh(mesh);
+
+        //Erstellung eines Materials und dessen Komponente
+        let material: fudge.Material = new fudge.Material("Solidwhite", fudge.ShaderUniColor, new fudge.CoatColored(new fudge.Color(1,0,0,1)));
+        let cmpMaterial: fudge.ComponentMaterial = new fudge.ComponentMaterial(material);
+
+        //sVerknüpfung der Komonenten mit dem Knoten
+        node.addComponent(cmpMesh);
+        node.addComponent(cmpMaterial);
+
+        //Kamera Komponente
+        let cmpCamera: fudge.ComponentCamera = new fudge.ComponentCamera();
+
+
         let viewport: fudge.Viewport = new fudge.Viewport;
-        viewport.initialize("Viewport", null, null, canvas);
-
-        
-
+        viewport.initialize("camera", node, cmpCamera, canvas);
+        //Verschiebung der Kamera
+        viewport.camera.pivot.translateZ(2);
+        fudge.Debug.log(viewport);
+        viewport.draw();
     }
 }
