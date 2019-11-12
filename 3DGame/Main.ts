8021937d9c58f600816ad1841eec718e2a3457d4
34275116 <34275116+NicoKlein97@users.noreply.github.com>
2019-11-12 12:20:21 +0100

    Block generator added

---
+++ 3DGame/Main.ts
@@ -0,0 +1,126 @@
+///<reference types="../FUDGE/Build/FudgeCore.js"/>
+namespace FudgeCraft {
+
+    import fudge = FudgeCore;
+
+    window.addEventListener("load", handleLoad);
+
+    export let viewport: fudge.Viewport;
+
+    let meshQuad: fudge.MeshQuad = new fudge.MeshQuad();
+    let allbuildingMatrices: any = [];
+
+    initializeBuildingMatrices();
+
+    let translationValues = [[-1, 1], [0, 1], [1, 1],
+                            [-1, 0], [0, 0], [1, 0],
+                            [-1, -1], [0, -1], [1, -1]];
+ 
+    function handleLoad(_event: Event): void {
+        const canvas: HTMLCanvasElement = document.querySelector("canvas");
+        fudge.RenderManager.initialize();
+        fudge.Debug.log(canvas);
+        
+        let cmpCamera: fudge.ComponentCamera = new fudge.ComponentCamera();
+        cmpCamera.pivot.translateZ(50);
+    
+        let game: fudge.Node = createGame();
+        viewport = new fudge.Viewport();
+        viewport.initialize("Viewport", game, cmpCamera, canvas);
+        fudge.Debug.log(viewport);
+
+        viewport.draw();
+
+        fudge.Loop.addEventListener(fudge.EVENT.LOOP_FRAME, update);
+        fudge.Loop.start();
+    }
+
+    function update(_event: Event): void {
+
+        fudge.RenderManager.update();
+        
+        viewport.draw();
+    }
+
+    function createGame(): fudge.Node {
+
+        let game: fudge.Node = new fudge.Node("Game");
+
+        buildBlocks(game);
+
+        return game;
+    }
+
+    function buildBlocks(_game: fudge.Node): void {
+
+        let translationTemp: number = -20;
+
+        for (let i: number = 0; i < allbuildingMatrices.length; i++) {
+
+            let baseBlock: fudge.Node = new fudge.Node("Base_Block_Fragment");
+            let mtrSoliColor: fudge.Material = new fudge.Material("SolidWhite", fudge.ShaderUniColor, new fudge.CoatColored(new fudge.Color(1, 0, 0, 0)));
+            baseBlock.addComponent(new fudge.ComponentMesh(meshQuad));
+            baseBlock.addComponent(new fudge.ComponentMaterial(mtrSoliColor));
+            baseBlock.addComponent(new fudge.ComponentTransform);
+            baseBlock.cmpTransform.local.translateX(translationTemp);
+
+            let buildingMtrx = allbuildingMatrices[i];
+
+            for (let j: number = 0; j < buildingMtrx.length; j++) {
+                if (buildingMtrx[j] == true) {
+                    let subBlock: fudge.Node = new fudge.Node("subBlock");
+                    subBlock.addComponent(new fudge.ComponentMesh(meshQuad));
+                    subBlock.addComponent(new fudge.ComponentMaterial(mtrSoliColor));
+                    subBlock.addComponent(new fudge.ComponentTransform);
+                    subBlock.cmpTransform.local.translateX(translationValues[j][0]);
+                    subBlock.cmpTransform.local.translateY(translationValues[j][1]);
+                    baseBlock.appendChild(subBlock);
+                    _game.appendChild(baseBlock);
+                }
+            }
+            translationTemp = translationTemp + 5; 
+        }
+        
+    }
+    
+    function initializeBuildingMatrices(): void{
+
+        let buildingMtrxIBlock: boolean[] = [false, true, false, 
+                                            false, false, false, 
+                                            false, true, false];
+        allbuildingMatrices.push(buildingMtrxIBlock);    
+
+        let buildingMtrx2x2Block: boolean[] = [false, false, false,
+                                            false, false, true,
+                                            false, true, true];
+        allbuildingMatrices.push(buildingMtrx2x2Block);
+
+        let buildingMtrxTBlock: boolean[] = [true, true, true, 
+                                            false, false, false, 
+                                            false, true, false];
+        allbuildingMatrices.push(buildingMtrxTBlock);
+
+        let buildingMtrxLBlock: boolean[] = [false, true, false, 
+                                            false, false, false, 
+                                            false, true, true];
+        allbuildingMatrices.push(buildingMtrxLBlock);
+
+        let buildingMtrxReversedLBlock: boolean[] = [false, true, false, 
+                                                    false, false, false, 
+                                                    true, true, false];
+        allbuildingMatrices.push(buildingMtrxReversedLBlock);
+
+        let buildingMtrxZBlock: boolean[] = [true, true, false, 
+                                            false, false, false, 
+                                            false, true, true];
+        allbuildingMatrices.push(buildingMtrxZBlock);
+
+        let buildingMtrxReversedZBlock: boolean[] = [false, true, true, 
+                                                    false, false, false, 
+                                                    true, true, false];
+        allbuildingMatrices.push(buildingMtrxReversedZBlock);
+
+    }
+    
+
+}
